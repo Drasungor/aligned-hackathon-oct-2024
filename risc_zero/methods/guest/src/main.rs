69a51956edd9bcb714af56f705ceaf3cc812c2d8
Agustin Cambiano <aguscambiano@gmail.com>
2024-10-12 00:51:47 -0300

    Start implementation of map and bug

--- risc_zero/methods/guest/src/main.rs
+++ risc_zero/methods/guest/src/main.rs
@@ -1,12 +1,57 @@
 use risc0_zkvm::guest::env;
 
+use serde::{Deserialize, Serialize};
+// use serde_derive::{Deserialize, Serialize};
+// use serde_json::to_string;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+enum BlockedTile {
+    TopLeft,
+    TopRight,
+    Left,
+    Right,
+    BottomLeft,
+    BottomRight,
+}
+
+struct Map {
+    line_length: usize,
+    lines: Vec<Vec<bool>>,
+}
+
+struct Bug {
+    map_ref: &Map,
+    horizontal: usize,
+    vertical: usize,
+}
+
+impl Bug {
+
+    fn new(map_ref: &Map, horizontal: usize, vertical: usize) -> Bug {
+        Bug { map_ref, horizontal: 4, vertical: 4 }
+    }
+
+    fn move() {
+
+    }
+
+}
+
+
 fn main() {
     // TODO: Implement your guest code here
 
     // read the input
-    let input: u32 = env::read();
+    let input: Vec<BlockedTile> = env::read();
 
-    // TODO: do something with the input
+    let mut cat_position = CatPosition { horizontal: 4, vertical: 4 };
+
+
+
+    for blocked_tile in &input {
+
+    }
 
     // write public output to the journal
     env::commit(&input);
