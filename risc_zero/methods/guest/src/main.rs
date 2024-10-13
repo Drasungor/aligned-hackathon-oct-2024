70129a5f3d55eb677e0e43346fc925659022b53b
Agustin Cambiano <aguscambiano@gmail.com>
2024-10-12 22:30:18 -0300

    Refactor memory management and start bug movement implementation

--- risc_zero/methods/guest/src/main.rs
+++ risc_zero/methods/guest/src/main.rs
@@ -4,11 +4,11 @@ use serde::{Deserialize, Serialize};
 // use serde_derive::{Deserialize, Serialize};
 // use serde_json::to_string;
 
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 
 #[derive(Serialize, Deserialize, Debug)]
-enum BlockedTile {
+enum MovingDirection {
     TopLeft,
     TopRight,
     Left,
@@ -27,7 +27,8 @@ struct Position {
 
 struct Map {
     line_length: usize,
-    lines: Vec<Vec<bool>>,
+    // lines: Vec<Vec<bool>>,
+    lines: Rc<RefCell<Vec<Vec<bool>>>>,
 }
 
 impl Map {
@@ -56,14 +57,26 @@ impl Map {
         lines.push(Map::build_tiles_line(line_length, vec![3, 8, 9]));
         lines.push(Map::build_tiles_line(line_length, vec![1, 9]));
 
-        Map { line_length, lines }
+        // Map { line_length, lines }
+        Map { line_length, lines: Rc::new(RefCell::new(lines)) }
     }
 
     pub fn block_tile(&mut self, position: &Position) {
-        assert!((position.horizontal < self.line_length) && (position.vertical < self.lines.len()), "Out of bounds position");
-        assert!((!self.lines[position.vertical][position.horizontal]), "Position already blocked");
-        self.lines[position.vertical][position.horizontal] = true;
+        let mut lines_ref = self.lines.borrow_mut();
+        // assert!((position.horizontal < self.line_length) && (position.vertical < self.lines.len()), "Out of bounds position");
+        // assert!((!self.lines[position.vertical][position.horizontal]), "Position already blocked");
+        // self.lines[position.vertical][position.horizontal] = true;
+
+        assert!((position.horizontal < self.line_length) && (position.vertical < lines_ref.len()), "Out of bounds position");
+        assert!((!lines_ref[position.vertical][position.horizontal]), "Position already blocked");
+        lines_ref[position.vertical][position.horizontal] = true;
+
     }
+
+    pub fn get_current_map_state(&self) -> Ref<'_, Vec<Vec<bool>>> {
+        self.lines.borrow()
+    }
+
 }
 
 
@@ -82,14 +95,17 @@ impl Bug {
         Bug { map_ref, current_position: initial_position }
     }
 
-    pub fn move_tile() {
-
+    pub fn move_tile(&mut self) {
+        // self.map_ref.borrow()
+        let map_ref = self.map_ref.borrow();
+        let current_map_state = map_ref.get_current_map_state();
     }
 
     pub fn is_at_position(&self, position: &Position) -> bool {
         // horizontal == self.horizontal && vertical == self.vertical
         self.current_position == *position
     }
+
 
 }
 
