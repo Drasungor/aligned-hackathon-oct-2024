a57a916ae07117a7e66dd2bfdfd200df6e70af16
Marcos Rolando <marcosrolando.mr@gmail.com>
2024-10-15 22:21:28 -0300

    Add singleton with game state

--- game/rust/src/lib.rs
+++ game/rust/src/lib.rs
@@ -1,8 +1,41 @@
-use godot::prelude::*;
+use game::GameContainer;
+use godot::{classes::Engine, prelude::*};
 
 mod entities;
+mod game;
 
 struct MyExtension;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {}
+unsafe impl ExtensionLibrary for MyExtension {
+    fn on_level_init(level: InitLevel) {
+        if level == InitLevel::Scene {
+            // The StringName identifies your singleton and can be
+            // used later to access it.
+            Engine::singleton().register_singleton(
+                StringName::from("GameContainer"),
+                GameContainer::new_alloc().upcast::<GameContainer>(),
+            );
+        }
+    }
+
+    fn on_level_deinit(level: InitLevel) {
+        if level == InitLevel::Scene {
+            // Get the `Engine` instance and `StringName` for your singleton.
+            let mut engine = Engine::singleton();
+            let singleton_name = StringName::from("GameContainer");
+
+            // We need to retrieve the pointer to the singleton object,
+            // as it has to be freed manually - unregistering singleton 
+            // doesn't do it automatically.
+            let singleton = engine
+                .get_singleton(singleton_name.clone())
+                .expect("cannot retrieve the singleton");
+
+            // Unregistering singleton and freeing the object itself is needed 
+            // to avoid memory leaks and warnings, especially for hot reloading.
+            engine.unregister_singleton(singleton_name);
+            singleton.free();
+        }
+    }
+}
