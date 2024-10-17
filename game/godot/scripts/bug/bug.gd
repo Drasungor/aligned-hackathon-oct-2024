39290521dbef293004300a8e798ae8cf2bc7156b
nravesz <nraveszani@gmail.com>
2024-10-16 21:43:23 -0300

    Bug color (#4)
    * add colored sprites, background and tiles
    
    * add sprites
    
    * change all sprites

--- game/godot/scripts/bug/bug.gd
+++ game/godot/scripts/bug/bug.gd
@@ -17,7 +17,7 @@ const DISTANCE_TOLERANCE := 0.1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	bug_sprite.play_bottom_left_animation();
 
 
 func _physics_process(delta: float) -> void:
