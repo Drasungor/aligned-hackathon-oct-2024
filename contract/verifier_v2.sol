3f846e981c98b54fd0cc3122b22f1103ea9de817
Agustin Cambiano <aguscambiano@gmail.com>
2024-10-22 20:54:57 -0300

    Update contract v2

--- contract/verifier_v2.sol
+++ contract/verifier_v2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-contract TrapTheBugRecordsTest {
+contract TrapTheBugValidator {
 
     struct RecordHolder {
         uint32 stepsAmount;
@@ -9,9 +9,14 @@ contract TrapTheBugRecordsTest {
         uint32 updatesCounter;
     }
 
+    address public alignedServiceManager;
+    address public paymentServiceAddr;
     RecordHolder[] public recordHolders;
     address owner;
     uint32 updatesCounter;
+
+    // TODO: have an owner who can update a stored storedRecordsLimit value
+    uint32 public storedRecordsLimit = 10;
 
     bytes32 public trapTheBugProgramIdCommitment =
         0x745e39c4866b503b4f5dc41ae3a15d4038497f6c80a87644f5789e2bd3e1a8ab;
@@ -19,33 +24,85 @@ contract TrapTheBugRecordsTest {
     event NewRecordHolder(address indexed holder, uint32 newRecord);
 
     // constructor(uint32 _storedRecordsLimit, address _alignedServiceManager, address _paymentServiceAddr) {
-    constructor() {
+    constructor(address _alignedServiceManager, address _paymentServiceAddr) {
+        alignedServiceManager = _alignedServiceManager;
+        paymentServiceAddr = _paymentServiceAddr;
         owner = msg.sender;
         // storedRecordsLimit = _storedRecordsLimit;
         updatesCounter = 0;
+    }
 
-        // TODO: have an owner who can update a stored storedRecordsLimit value
-        uint32 storedRecordsLimit = 10;
-        for(uint i = 0; i < storedRecordsLimit; i++) {
-            recordHolders.push(RecordHolder(type(uint32).max, address(0), updatesCounter));
-        }
+    function verifyBatchInclusion(
+        bytes32 proofCommitment,
+        bytes32 pubInputCommitment,
+        bytes32 programIdCommitment,
+        bytes20 proofGeneratorAddr,
+        bytes32 batchMerkleRoot,
+        bytes memory merkleProof,
+        uint256 verificationDataBatchIndex,
+        bytes memory pubInputBytes
+    ) public returns (bool) {
+        require(
+            trapTheBugProgramIdCommitment == programIdCommitment,
+            "Program ID doesn't match"
+        );
+
+        require(
+            pubInputCommitment == keccak256(abi.encodePacked(pubInputBytes)),
+            "Steps amount doesn't match with public input"
+        );
+
+        (
+            bool callWasSuccessful,
+            bytes memory proofIsIncluded
+        ) = alignedServiceManager.staticcall(
+                abi.encodeWithSignature(
+                    "verifyBatchInclusion(bytes32,bytes32,bytes32,bytes20,bytes32,bytes,uint256,address)",
+                    proofCommitment,
+                    pubInputCommitment,
+                    programIdCommitment,
+                    proofGeneratorAddr,
+                    batchMerkleRoot,
+                    merkleProof,
+                    verificationDataBatchIndex,
+                    paymentServiceAddr
+                )
+            );
+
+        require(callWasSuccessful, "static_call failed");
+        uint32 stepsAmount = bytesToUint32(pubInputBytes);
+        updateRecordsList(stepsAmount);
+        return abi.decode(proofIsIncluded, (bool));
+    }
+
+    function bytesToUint32(
+        bytes memory data
+    ) public pure returns (uint32) {
+        require(data.length >= 4, "Input bytes must be at least 4 bytes long");
+
+        uint32 steps_amount = uint32(uint8(data[0])) |
+            (uint32(uint8(data[1])) << 8) |
+            (uint32(uint8(data[2])) << 16) |
+            (uint32(uint8(data[3])) << 24);
+
+        return steps_amount;
     }
 
     function updateRecordsList(
-        address receivedAddress, uint32 stepsAmount
-    ) public {
+        uint32 stepsAmount
+    ) private {
+        if (recordHolders.length < storedRecordsLimit) {
+            // If the record holders limit was not reached we add a new record holder                
+            recordHolders.push(RecordHolder(stepsAmount, msg.sender, updatesCounter));
+            updatesCounter++;
+            return;
+        }
         uint maxStepsRecordIndex = 0;
         for(uint i = 0; i < recordHolders.length; i++) {
             RecordHolder memory currentRecordHolder = recordHolders[i];
-            if (currentRecordHolder.recordHolder == address(0)) {
-                // If the record holders limit was not reached we initialize the next
-                // available slot                
-                recordHolders[i] = RecordHolder(stepsAmount, receivedAddress, updatesCounter);
-                updatesCounter++;
-                return;
-            } else if (currentRecordHolder.recordHolder == receivedAddress) {
+            if (currentRecordHolder.recordHolder == msg.sender) {
                 if (currentRecordHolder.stepsAmount > stepsAmount) {
-                    recordHolders[i] = RecordHolder(stepsAmount, receivedAddress, updatesCounter);
+                    recordHolders[i] = RecordHolder(stepsAmount, msg.sender, updatesCounter);
                     updatesCounter++;
                 }
                 return;
@@ -64,8 +121,12 @@ contract TrapTheBugRecordsTest {
         // currently worst and latest record, which is not the same wallet as the caller
         RecordHolder memory worstRecordHolder = recordHolders[maxStepsRecordIndex];
         if (stepsAmount < worstRecordHolder.stepsAmount) {
-            recordHolders[maxStepsRecordIndex] = RecordHolder(stepsAmount, receivedAddress, updatesCounter);
+            recordHolders[maxStepsRecordIndex] = RecordHolder(stepsAmount, msg.sender, updatesCounter);
             updatesCounter++;
         }
+    }
+
+    function getRecordHoldersLength() public view returns (uint) {
+        return recordHolders.length;
     }
 }
