59ec7f3f1c6fd2a011dc07d084d78d58051ce8c9
Agustin Cambiano <aguscambiano@gmail.com>
2024-10-23 20:12:04 -0300

    Finish ethereum record holders getter

--- game/rust/src/ethereum.rs
+++ game/rust/src/ethereum.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use std::vec;
 
 use ethers::prelude::*;
 use ethers::providers::{Http, Provider};
@@ -6,8 +7,14 @@ abigen!(BugVerificationContract, "../../risc_zero/host/bug_verification.json",);
 
 const RPC_URL: &str = "https://ethereum-holesky-rpc.publicnode.com";
 
+#[derive(Debug)]
+pub struct RecordHolder {
+    stepsAmount: u32,
+    recordHolder: String,
+    updatesCounter: u32,
+}
 
-fn get_record_holders() {
+fn get_record_holders() -> Vec::<RecordHolder> {
 
     let rt = tokio::runtime::Runtime::new().expect("Error in tokio runtime generation");
     let provider = Provider::<Http>::try_from(RPC_URL).expect("Failed to connect to provider");
@@ -23,13 +30,25 @@ fn get_record_holders() {
         // Instantiate the contract instance with the provider (no wallet needed)
         let contract = BugVerificationContract::new(contract_address, provider_arc);
 
-        let index: u64 = 1;
-        let result = contract.record_holders(index.into()).call().await.expect("Error getting index");
+        let record_holders_amount: u32 = contract.get_record_holders_length().call().await.expect("Error getting array length").as_u32();
 
-        println!("Value at index {}: {:?}", index, result);
+        let mut record_holders = Vec::<RecordHolder>::new();
+        for i in 0..record_holders_amount {
+            let current_record_holder: (u32, H160, u32) = contract.record_holders(i.into()).call().await.expect("Error getting indexed value");
+            record_holders.push(RecordHolder {
+                stepsAmount: current_record_holder.0,
+                recordHolder: current_record_holder.1.to_string(),
+                updatesCounter: current_record_holder.2,
+            })
+        }
 
+        // println!("Array length: {}", record_holders_amount);
 
-        // Ok(())
+        // let index: u64 = 0;
+
+        // println!("Value at index {}: {:?}", index, result);
+        println!("record_holders: {:?}", record_holders);
+
+        record_holders
     })
-
 }
