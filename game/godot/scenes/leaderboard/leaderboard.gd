0d7a41902890df5b59f3c98311e45f3e6e0231bb
nravesz <nraveszani@gmail.com>
2024-10-24 20:54:17 -0300

    add sort

--- game/godot/scenes/leaderboard/leaderboard.gd
+++ game/godot/scenes/leaderboard/leaderboard.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	"ElBichitoAlineado",
 	"0x2981e1dD10168a1187Cf39D36b48465715Aab85D",
 	"30");
-
+	print(get_data());
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -32,3 +32,29 @@ func add_item(
 	var leaderboard_item: LeaderboardItem = leaderboard_item_scene.instantiate()
 	leaderboard_item.set_data(rank, nickname, wallet, steps);
 	leaderboard_list.add_child(leaderboard_item);
+
+
+func get_data() -> Array:
+	var records: Array = GameContainer.get_leaderboad();
+	#var records := [
+	#{ "steps_amount": 5, "record_holder": "0x2981…b85d", "updates_counter": 1 },
+	#{ "steps_amount": 3, "record_holder": "0x2981…b85d", "updates_counter": 2 },
+	#{ "steps_amount": 5, "record_holder": "0x2981…b85d", "updates_counter": 3 },
+	#]
+	
+	var callable: Callable = Callable(self, "compare_records")
+	records.sort_custom(callable);
+	return records;
+
+
+func compare_records(a: Dictionary, b: Dictionary) -> int:
+	if a["steps_amount"] > b["steps_amount"]:
+		return -1
+	elif a["steps_amount"] < b["steps_amount"]:
+		return 1
+	else:
+		if a["updates_counter"] > b["updates_counter"]:
+			return -1
+		elif a["updates_counter"] < b["updates_counter"]:
+			return 1
+	return 0
