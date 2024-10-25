ee75fb786b508124dcafc09e0c770ec6313e8056
nravesz <nraveszani@gmail.com>
2024-10-24 21:54:00 -0300

    add data to the leaderboard

--- game/godot/scenes/leaderboard/leaderboard.gd
+++ game/godot/scenes/leaderboard/leaderboard.gd
@@ -8,19 +8,7 @@ class_name Leaderboard
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_item("#1",
-	"0x2981e1dD10168a1187Cf39D36b48465715Aab85D",
-	"30",
-	"5");
-	add_item("#1",
-	"0x2981e1dD10168a1187Cf39D36b48465715Aab85D",
-	"30",
-	"6");
-	print(get_data());
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	add_data()
 
 
 func add_item(
@@ -34,13 +22,19 @@ func add_item(
 	leaderboard_list.add_child(leaderboard_item);
 
 
+func add_data() -> void:
+	var records: Array = get_data();
+	for i : int in range(records.size()):
+		add_item(
+			"#%d" % (i + 1),
+			records[i]["record_holder"],
+			str(records[i]["steps_amount"]),
+			str(records[i]["updates_counter"])
+		)
+
+
 func get_data() -> Array:
 	var records: Array = GameContainer.get_leaderboad();
-	#var records := [
-	#{ "steps_amount": 5, "record_holder": "0x2981…b85d", "updates_counter": 1 },
-	#{ "steps_amount": 3, "record_holder": "0x2981…b85d", "updates_counter": 2 },
-	#{ "steps_amount": 5, "record_holder": "0x2981…b85d", "updates_counter": 3 },
-	#]
 	
 	var callable: Callable = Callable(self, "compare_records")
 	records.sort_custom(callable);
