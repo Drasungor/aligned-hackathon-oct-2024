7a19dc482e7749a682f8bc2b37de15ed6d7a1103
nravesz <nraveszani@gmail.com>
2024-10-26 15:01:13 -0300

    fix back not working after winning/losing

--- game/godot/scenes/game/game.gd
+++ game/godot/scenes/game/game.gd
@@ -4,25 +4,24 @@ extends Node2D
 @onready var level_scene := preload("res://scenes/level/level.tscn");
 @onready var leaderboard_scene := preload("res://scenes/leaderboard/Leaderboard.tscn");
 
-@onready var scenes_canva_layer := $ScenesCanvaLayer/CenterContainer;
+@onready var scenes_canva_layer := $UICanvaLayer/CenterContainer;
 
-@onready var end_game_menu := $EndGameMenuCanvasLayer/CenterContainer/EndGameMenu;
-@onready var end_game_menu_canvas := $EndGameMenuCanvasLayer;
-@onready var end_game_menu_reset_button := $EndGameMenuCanvasLayer/CenterContainer/EndGameMenu/VBoxContainer/ResetGameButton;
+@onready var end_game_menu := $UICanvaLayer/CenterContainer/EndGameMenu;
+@onready var end_game_menu_reset_button := $UICanvaLayer/CenterContainer/EndGameMenu/VBoxContainer/ResetGameButton;
 
-@onready var back_to_menu_button := $BackToMenuCanvasLayer/BackToMenuButton;
+@onready var back_to_menu_button := $UICanvaLayer/BackToMenuButton;
 
 var current_scene: Node = null;
 
 func _ready() -> void:
-	end_game_menu_canvas.visible = false;
+	end_game_menu.visible = false;
 	show_menu();
 	back_to_menu_button.pressed.connect(self._on_back_to_menu_pressed);
 	end_game_menu_reset_button.pressed.connect(self._on_end_game_menu_reset_pressed);
 
 
 func show_menu() -> void:
-	end_game_menu_canvas.visible = false;
+	end_game_menu.visible = false;
 	if current_scene:
 		current_scene.queue_free();
 	current_scene = menu_scene.instantiate();
@@ -36,7 +35,7 @@ func show_menu() -> void:
 
 
 func show_level() -> void:
-	end_game_menu_canvas.visible = false;
+	end_game_menu.visible = false;
 	if current_scene:
 		current_scene.queue_free();
 	GameContainer.reset();
@@ -47,7 +46,7 @@ func show_level() -> void:
 
 
 func show_leaderboard() -> void:
-	end_game_menu_canvas.visible = false;
+	end_game_menu.visible = false;
 	if current_scene:
 		current_scene.queue_free();
 	current_scene = leaderboard_scene.instantiate();
@@ -77,4 +76,4 @@ func _on_end_game_menu_reset_pressed() -> void:
 
 func _on_level_game_ended(player_won: bool) -> void:
 	end_game_menu.display_game_result(player_won);
-	end_game_menu_canvas.visible = true;
+	end_game_menu.visible = true;
