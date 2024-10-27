f360c7790d5650e16340a31fbecfe331e16a047b
nravesz <nraveszani@gmail.com>
2024-10-27 08:54:55 -0300

    change ui in menu

--- game/godot/scenes/menu/menu.gd
+++ game/godot/scenes/menu/menu.gd
@@ -1,13 +1,16 @@
 extends Control
 
 
-@onready var start_button := $Buttons/StartButton;
-@onready var leaderboard_button := $Buttons/LeaderboardButton;
-@onready var quit_button := $Buttons/QuitButton;
+@onready var start_button := $HBoxContainer/VBoxContainer/Buttons/StartButton;
+@onready var leaderboard_button := $HBoxContainer/VBoxContainer/Buttons/LeaderboardButton;
+@onready var quit_button := $HBoxContainer/VBoxContainer/Buttons/QuitButton;
 
+@onready var bug := $HBoxContainer/Panel/MenuBug;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_button.set_text("Start");
 	leaderboard_button.set_text("Leaderboard")
 	quit_button.set_text("Quit")
+	
+	bug.play();
